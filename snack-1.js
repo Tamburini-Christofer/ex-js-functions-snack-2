afc77aec7f8930e17091e394b0911812cb337379
Tamburini-Christofer <christofer.tamburini92@gmail.com>
2025-12-12 09:12:38 +0100

    Snack 1

---
+++ snack-1.js
@@ -0,0 +1,13 @@
+//Questa è una funzione dichiarativa
+function somma (num1, num2) {
+    return(num1 + num2)
+};
+
+//Questa è una funzione anonima assegnata a una variabile
+const somma = (num1, num2) => {
+    return(num1 + num2)
+};
+
+//Questa è una funzione anonima assegnata a una variabile su una sola riga
+const somma = (num1, num2) => num1 + num2;
+
